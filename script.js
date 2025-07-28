74b84eccfd52830bcfef3bdc512330d433eeb7b4
Алина <152898117+dfgjg@users.noreply.github.com>
2025-07-28 10:49:10 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 const swiper = new Swiper('.swiper', {
     loop:true,
-    pagination:{
-        el:'.swiper-pagination',
-        clickable: true,
-    },
+    
     navigation:{
         nextEl:'.swiper-btn-next',
         prevEl:'.swiper-btn-prev',
