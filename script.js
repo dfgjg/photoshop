80145f49f28a59ca2759a7f1c26fb41711c68411
Алина <152898117+dfgjg@users.noreply.github.com>
2025-08-01 09:50:40 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -12,11 +12,33 @@ const swiper = new Swiper('.swiper', {
 })
 let goods = document.querySelector('.goods img')
 let catsBucket = document.querySelectorAll('.add-to-bucket')
+let catsBucket2 = document.querySelectorAll('.add-to-bucket2')
+let catsBucket3 = document.querySelectorAll('.add-to-bucket3')
+let catsBucket4 = document.querySelectorAll('.add-to-bucket4')
 let price = document.querySelector('.goods-price')
 let count = document.querySelector('.count-span')
 
 for (let i = 0; i<catsBucket.length; i+=1){
     catsBucket[i].addEventListener('click',function(){
         count.innerHTML = +count.innerHTML + 1
+        price.innerHTML = +price.innerHTML + 1000
+    })
+}
+for (let i = 0; i<catsBucket2.length; i+=1){
+    catsBucket2[i].addEventListener('click',function(){
+        count.innerHTML = +count.innerHTML + 1
+        price.innerHTML = +price.innerHTML + 500
+    })
+}
+for (let i = 0; i<catsBucket3.length; i+=1){
+    catsBucket3[i].addEventListener('click',function(){
+        count.innerHTML = +count.innerHTML + 1
+        price.innerHTML = +price.innerHTML + 700
+    })
+}
+for (let i = 0; i<catsBucket4.length; i+=1){
+    catsBucket4[i].addEventListener('click',function(){
+        count.innerHTML = +count.innerHTML + 1
+        price.innerHTML = +price.innerHTML + 200
     })
 }
