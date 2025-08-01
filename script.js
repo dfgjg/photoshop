5fb1e4933d5f377f62b526d57f50baf22228126c
Алина <152898117+dfgjg@users.noreply.github.com>
2025-08-01 09:43:58 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -10,3 +10,13 @@ const swiper = new Swiper('.swiper', {
         disableOnInteraction:false,
     }
 })
+let goods = document.querySelector('.goods img')
+let catsBucket = document.querySelectorAll('.add-to-bucket')
+let price = document.querySelector('.goods-price')
+let count = document.querySelector('.count-span')
+
+for (let i = 0; i<catsBucket.length; i+=1){
+    catsBucket[i].addEventListener('click',function(){
+        count.innerHTML = +count.innerHTML + 1
+    })
+}
